baa06274844c71bbb4f69f38740d3283e1c6848f
fadd00 <andhikahutama9@gmail.com>
2025-10-23 08:27:52 +0700

    implement ActivitasPertama composable and add related strings; initialize project configuration files

--- app/src/main/java/com/sample/questadvancelayout_239/UIPertama.kt
+++ app/src/main/java/com/sample/questadvancelayout_239/UIPertama.kt
@@ -1,4 +1,24 @@
 package com.sample.questadvancelayout_239
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+@composable
+fun ActivitasPertama(modifier: Modifier){
+    Column { modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally ){
+        Text(text = stringResource(id = R.string.prodi),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold)
+    }
+}
+
 class UIPertama {
 }
